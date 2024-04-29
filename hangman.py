2a6a182497afae546d9671dc354fba08266dad03
M.Macgregor <wyconomad@gmail.com>
2024-04-29 13:03:54 -0600

    added loop command

--- hangman.py
+++ hangman.py
@@ -8,10 +8,10 @@ wordDictionary = ('Elephant, Rainbow, Computer, Butterfly, Chocolate, Adventure,
 ######choose a random word
 randomWord = random.choice(wordDictionary)
 
-for x in randomWord
-    print("_", end=' ')
+for x in randomWord:
+    print("_", end=" ")
 
-    def print_hangman(wrong):
+def print_hangman(wrong):
     if(wrong == 0):
         print("\n+---+")
         print("    |")
@@ -21,26 +21,26 @@ for x in randomWord
     elif(wrong == 1):
         print("\n+---+")
         print(" O   |")
-        print("    |")
-        print("    |")
+        print("     |")
+        print("     |")
         print("   ===")
     elif(wrong == 2):
         print("\n+---+")
-        print(" O    |")
+        print(" O   |")
         print(" |   |")
-        print("    |")
+        print("     |")
         print("   ===")
     elif(wrong == 3):
         print("\n+---+")
         print(" O   |")
         print("/|   |")
-        print("    |")
+        print("     |")
         print("   ===")
     elif(wrong == 4):
         print("\n+---+")
         print(" O   |")
-        print("/|\   |")
-        print("    |")
+        print("/|\  |")
+        print("     |")
         print("   ===")
     elif(wrong == 5):
         print("\n+---+")
@@ -51,23 +51,36 @@ for x in randomWord
     elif(wrong == 6):
         print(" O   |")
         print("/|\  |")
-        print("/ \   |")
+        print("/ \  |")
         print("   ===")
 
 def printWord(guessedLetter):
     counter=0
-    rightLetter=0
-        for char in randomWord:
+    rightLetters=0
+    for char in randomWord:
             if(char in guessedLetter):
                 print(randomWord[counter],end=" ")
                 rightLetters+=1 
             else:
                 print(" ",end="")
                 counter+=1 
-            return rightLetter
+            return rightLetters
     
 
     def printLines():
         print("\r")
         for char in randomWord:
-            print("\u203",end="")
+            print("\u203e",end="")
+
+            length_of_word_to_guess = len(randomWord)
+            amount_of_times_wrong = 0
+            current_guess_index = 0
+            current_letters_guessed = []
+            current_letters_right = 0
+
+            while(amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_guess):
+                print("\nLetters guess so far: " )
+                for letter in current_letters_guessed:
+                    print(letter, end=" ")
+            ##Prompt user for imput
+            letterGuessed = input("\nGuess a letter:")
