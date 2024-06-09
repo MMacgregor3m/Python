b0962a2c32ab2d264b36db04d57ae5c0f705f743
Cyb3rGhost70 <wyconomad@gmail.com>
2024-06-09 15:54:14 -0600

    added more code

--- ##hangman II.py
+++ ##hangman II.py
@@ -6,3 +6,12 @@ word = random.choice(animals).lower()
 
 guessed_correctly = []
 guessed_incorrectly = []
+tries = 6
+hangman_count=-1
+while tries > 0:
+    output = ''
+    for letter in word:
+        if letter in guessed_correctly
+            output += letter
+    else:
+        output += '_ '
