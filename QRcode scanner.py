a18821c971d3b95faab5ac57f214b314f2b6e893
Cyb3rGhost70 <wyconomad@gmail.com>
2024-07-26 17:36:06 -0600

    updated QR code

--- QRcode scanner.py
+++ QRcode scanner.py
@@ -1,5 +1,5 @@
 # qrcode scanner
 import qrcode
 
-myqr = qrcode.make("https://www.youtube.com/@Wyco_Nomad")
-myqr.save("myqr.png")
+myqr = qrcode.make("https://jasemedical.com/?rstr=14870")
+myqr.save("jace.png")
--- portscanner.py
+++ portscanner.py
@@ -6,5 +6,10 @@ s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host= "137.74.187.100"
 port= 443
 
-def postScanner (porty):
-    if s.connect_ex
+def portScanner (port):
+    if s.connect_ex((host,port)):
+        print("The port is closed")
+    else:
+        print ("The port is open")
+
+portScanner(port)
